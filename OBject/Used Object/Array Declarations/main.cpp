aa62c236001a552b8854d423c49158d281e1fbe6
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-02 01:10:42 +0530

    Create main.cpp

---
+++ OBject/Used Object/Array Declarations/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+//Declaring an integer array of 10 elements 
+int *k;
+k = new int [10]; //an array of 10 integers
+if (!k){
+cout << "Cannot Allocate Memory" << endl;
+exit(1);
+}
+delete [] k;
+
+}
