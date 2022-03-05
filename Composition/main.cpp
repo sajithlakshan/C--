8e5f2f98c79479910734a196217441be954e4c01
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-05 11:10:52 +0530

    Create main.cpp

---
+++ Composition/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// Part
+class Room {
+
+private:
+  	int roomNo;
+
+public:
+	Room() {}
+
+	Room(int no) {
+		roomNo = no;
+	}
+
+	void Display() {
+		cout << "Room : " << roomNo << endl;
+	}
+
+	~Room() {
+		cout << "Destroying Room : " << roomNo << endl;
+	}
+
+};
+
+
+// Whole
+class House {
+
+private:
+  	Room *room[2];
+
+public:
+	House() {
+		room[0] = new Room(101);
+		room[1] = new Room(102);
+	}
+
+	House(int no1, int no2) {
+		room[0] = new Room(no1);
+		room[1] = new Room(no2);
+	}
+
+	void DisplayClassRooms() {
+		for (int i=0; i<2; i++) {
+			room[i]->Display();
+		}
+	}
+
+	~House() {
+		cout << "House is destroyed!" << endl;
+		for (int i=0; i < 2; i++) {
+			delete room[i];
+		}
+	}
+};
+
+
+// main
+int main() {
+	//If One House
+    cout << "__________First House__________"<< endl;
+	House *myHouse1;
+
+	myHouse1 = new House();
+	myHouse1->DisplayClassRooms();
+	delete myHouse1;
+	
+    cout << "__________2 House__________"<< endl;	
+	//If two House
+	 House *myHouse2;
+	
+	 myHouse2 = new House(501, 502);
+	 myHouse2->DisplayClassRooms();
+	 delete myHouse2;
+
+	return 0;
+}
