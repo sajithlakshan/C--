66555dd31a6bd26930c19949575b9791de72726b
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-05 17:15:29 +0530

    Create main.cpp

---
+++ Object Oriented Concepts/Polymorphism/Lec_EX/STEP_2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+class Animal {
+
+public:
+	Animal() {}
+
+	virtual void speak() {}
+
+};
+
+class Cat : public Animal {
+public:
+
+	Cat(char tname[]) : Animal() { 
+    cout << "Meow... Meow..." << endl;}
+
+};
+
+class Dog : public Animal {
+public:
+
+	Dog(char tname[]): Animal() { 
+    cout << "Bow... Bow..." << endl;
+    }
+
+};
+class Cow: public Animal {
+public:
+
+	Cow(char tname[]) : Animal() {
+    cout << "Moo... Moo..." << endl;}
+
+};
+int main()
+{
+	Animal *ani[4];
+	ani[0] = new Cat("Micky the Cat");
+	ani[1] = new Dog("Rover the Dog");
+	ani[2] = new Cow("roo the Cow");
+	
+
+
+return 0;
+}
