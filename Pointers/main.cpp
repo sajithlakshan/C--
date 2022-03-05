d58bbff2b135e040fdbb74dcf4216b4546adbd3f
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-03-05 09:43:12 +0530

    Create main.cpp

---
+++ Pointers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+	
+	 int x = 5;		// 0x7ffd1da6e24c
+	 int *y;
+	 y = &x;
+	 cout << *y << endl;
+	 cout << y << endl;
+
+
+	return 0;
+}
